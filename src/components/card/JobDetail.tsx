6b5be036f0cb57bd88b165f9efe4a8a741edc901
muhammadraazy <muhammadrazi51@gmail.com>
2024-10-23 07:53:37 -0700

    added a job detail info

--- src/components/card/JobDetail.tsx
+++ src/components/card/JobDetail.tsx
@@ -15,7 +15,6 @@ const JobDetail: FC<IProps> = ({ jobId, showModal, setShowModal }) => {
 
    const filterItems = () => {
       const selectedItem = jobs.filter(job => job.id == jobId)
-      console.log(selectedItem)
       return selectedItem;  
    }
 
@@ -31,20 +30,27 @@ const JobDetail: FC<IProps> = ({ jobId, showModal, setShowModal }) => {
       <article className="h-[90%] absolute bottom-0 w-full bg-secondary rounded-tr-[1rem] rounded-tl-[1rem] py-[40px] px-[80px]">
          { filterItems().map(job => (
             <aside key={job.id} className="w-[70%]">
-                <div className="flex-center">
+                <div className="flex-center w-full">
                    <h2 className={`text-[16px] md:text-[18px] lg:text-[24px] font-bold`}> { job.title } </h2>
                    {/* { apply buttons } */}
                 </div>
                 <div className="flex-center gap-[10px] mt-[30px]">
                     <Image src={google} alt="company-logo" className="w-[70px] h-[70px] rounded-[15px] object-fill" />
                     <div className="flex flex-col gap-[10px]">
-                        <h4 className="text-main text-[14px] md:text-[16px] lg:text-[18px] font-bold"> {job['company-name']} </h4>
+                        <h4 className="text-main text-[14px] md:text-[16px] lg:text-[18px] font-bold"> { job['company-name'] } </h4>
                         <div className="flex-center gap-[10px]">
                           <p className="job-info-text"> { job.workplace  } </p>
                           <p className="job-info-text"> { job["work-type"]  } </p>
                           <p className="job-info-text"> { job.exp  } Years </p>
                         </div>
                     </div>
+                </div>
+
+                <div className={`flex flex-col mt-[30px] w-full`}>
+                    <aside className="w-full">
+                       <h3 className="font-bold text-[14px] md:text-[16px] lg:text-[18px]"> About this role </h3>
+                       <p className="mt-[10px] font-normal text-[2vmin]"> { job.aboutRole } </p>
+                    </aside>
                 </div>
             </aside>
          )) }
